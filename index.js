7a67fb6672ecd7c6a1335d1cc625468e8a4a5a4a
Sarfaraz Ahmed <imsarfy@gmail.com>
2018-08-26 19:47:07 +0530

    Deployed on AWS

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require("express"),
   app = express(),
   socket = require("socket.io");
 
-var server = app.listen(3000, function() {
-  console.log("App started on port 3000");
+var server = app.listen(process.env.PORT || 8081, function() {
+  console.log("App started on port 8081");
 });
 
 app.use(express.static("public"));
